44995ee83423c8ab9969a455df6e270d8cd01d81
小马同学 <89441679+m2075498242@users.noreply.github.com>
2024-04-29 14:30:45 +0800

    seo优化配置

--- src/app.vue
+++ src/app.vue
@@ -1,3 +1,11 @@
+<script setup lang="ts" >
+// 用于SEO优化
+useSeoMeta({
+  title: '哔哩哔哩 (゜-゜)つロ 干杯~-bilibili官方', //标题
+  description: '哔哩哔哩(bilibili.com)是国内知名的视频弹幕网站,这里有及时的动漫新番,活跃的ACG氛围,有创意的Up主。大家可以在这里找到许多欢乐', //网站描述
+  keywords: 'bilibili,哔哩哔哩,视频,弹幕,ACG,动漫,番剧,up主,Up主', //关键词搜索
+})
+</script>
 <template>
   <div>
     <!-- 相当于Router-view -->
